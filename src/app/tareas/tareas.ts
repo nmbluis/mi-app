ba216e73cde6a3f005eb55559488a1922969b402
nmbluis <luisfernandeznb@gmail.com>
2025-09-24 11:43:11 -0500

    Add Tareas component with template, styles, and tests; update app title

---
+++ src/app/tareas/tareas.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas',
+  imports: [],
+  templateUrl: './tareas.html',
+  styleUrl: './tareas.css'
+})
+export class Tareas {
+  tareas: string[] = [];
+  nuevaTarea: string= '';
+
+}
